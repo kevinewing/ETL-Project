38b88329cf01f3311ac91ea4ad6792ea40dff453
Kevin Ewing <kevinewing70@gmail.com>
2019-05-11 12:01:28 -0700

    Complete Export to SQL

---
+++ queries.sql
@@ -0,0 +1,13 @@
+-- Create and use state_db
+CREATE DATABASE state_db;
+USE state_db;
+
+drop table if exists state_data;
+
+-- Create tables for raw data to be loaded into
+CREATE TABLE state_data (
+  State varchar(20) PRIMARY KEY,
+  Births int,
+  Deaths int,
+  Zika_Cases int
+);
